84ec527d860817050c6fff27dd820dc25d72de56
LandonRGeorge <landon.robert.george@gmail.com>
2020-04-21 13:57:23 -0500

    Added dash datatable.

--- main.py
+++ main.py
@@ -9,146 +9,153 @@ import requests
 import pandas as pd
 import numpy as np
 
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+from dash.dependencies import Input, Output
+
 # Pandas display options
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', 100)
 
-url = "https://data.texas.gov/resource/naix-2893.json?"
+url_base = "https://data.texas.gov/resource/naix-2893.json?"
 
 
 # %%
 def df_cities():
-    query = f"""
+    url_args = f"""
         $select=distinct location_city&$limit=10000
     """
 
-    query = re.sub(r'\n\s*', '', query).replace(' ', '%20')
+    url_args = re.sub(r'\n\s*', '', url_args).replace(' ', '%20')
 
-    url_query = url + query
+    url_full = url_base + url_args
 
-    return pd.read_json(url_query).rename(columns={'location_city':'City'})
+    return pd.read_json(url_full).rename(columns={'location_city':'City'})
 
 df_cities = df_cities()
 df_cities.head()
 
 
+
+
 # %%
-query = f"""
-    $select=
-        location_name,
-        tabc_permit_number,
-        location_number,
-        location_address,
-        location_city,
-        min(obligation_end_date_yyyymmdd),
-        max(obligation_end_date_yyyymmdd),
-        sum(total_receipts),
-        avg(total_receipts),
-        sum(beer_receipts),
-        avg(beer_receipts),
-        sum(wine_receipts),
-        avg(wine_receipts),
-        sum(liquor_receipts),
-        avg(liquor_receipts)
-    &$group=
-        location_name,
-        tabc_permit_number,
-        location_number,
-        location_address,
-        location_city
-    &$where=
-        obligation_end_date_yyyymmdd between '2020-02-01' and '2020-04-30'
-        &location_city='{'DALLAS'.upper()}'
-    &$order=sum_liquor_receipts DESC
-    &$limit=100
-"""
 
-query = re.sub(r'\n\s*', '', query).replace(' ', '%20')
+def func_df_data(cities):
+    """Grab data from TX Comptroller API"""
 
-url_query = url + query
 
-df = (
-    pd.read_json(
-        url_query, 
-        convert_dates=['min_obligation_end_date_yyyymmdd', 'max_obligation_end_date_yyyymmdd']
-    ).assign(
-        min_obligation_beg_date_yyyymmdd = lambda x: x['min_obligation_end_date_yyyymmdd'] - pd.offsets.MonthBegin()
+    text_cities = ''
+    for i, city in enumerate(cities):
+        text_temp = f"'{city}'"
+        if i != len(cities) - 1:
+            text_temp+=','
+        text_cities+=text_temp
+
+    url_args = f"""
+        $select=
+            location_name,
+            tabc_permit_number,
+            location_number,
+            location_address,
+            location_city,
+            min(obligation_end_date_yyyymmdd),
+            max(obligation_end_date_yyyymmdd),
+            sum(total_receipts),
+            sum(beer_receipts),
+            sum(wine_receipts),
+            sum(liquor_receipts)
+        &$group=
+            location_name,
+            tabc_permit_number,
+            location_number,
+            location_address,
+            location_city
+        &$where=
+            obligation_end_date_yyyymmdd between '2020-02-01' and '2020-04-30'
+            and location_city in({text_cities})
+        &$order=sum_liquor_receipts DESC
+        &$limit=100
+    """
+
+
+    url_args = re.sub(r'\n\s*', '', url_args).replace(' ', '%20')
+
+    url_full = url_base + url_args
+
+    df = (
+        pd.read_json(
+            url_full, 
+            convert_dates=['min_obligation_end_date_yyyymmdd', 'max_obligation_end_date_yyyymmdd']
+        ).assign(
+            min_obligation_beg_date_yyyymmdd = lambda x: x['min_obligation_end_date_yyyymmdd'] - pd.offsets.MonthBegin()
+        )
     )
-)
 
-df.head()
+    cols = ({
+    'location_name': {'rename': 'LocName', 'dtype': 'object'},
+    'tabc_permit_number': {'rename': 'LicNbr', 'dtype': 'object'},
+    'location_number': {'rename': 'LocNbr', 'dtype': 'object'},
+    'location_address': {'rename': 'Address', 'dtype': 'object'},
+    'location_city': {'rename': 'City', 'dtype': 'object'},
+    'min_obligation_beg_date_yyyymmdd': {'rename': 'BegDateMin','dtype': 'datetime64'},
+    'min_obligation_end_date_yyyymmdd': {'rename': 'EndDateMin','dtype': 'datetime64'},
+    'max_obligation_end_date_yyyymmdd': {'rename': 'EndDateMax','dtype': 'datetime64'},
+    'sum_total_receipts': {'rename': 'TotalSum', 'dtype': 'float64'},
+    'sum_beer_receipts': {'rename': 'BeerSum', 'dtype': 'float64'},
+    'sum_wine_receipts': {'rename': 'WineSum', 'dtype': 'float64'},
+    'sum_liquor_receipts': {'rename': 'LiqSum', 'dtype': 'float64'},
+    })
 
-
-# %%
-cols = ({
-'location_name': {'rename': 'LocName', 'dtype': 'object'},
-'tabc_permit_number': {'rename': 'LicNbr', 'dtype': 'object'},
-'location_number': {'rename': 'LocNbr', 'dtype': 'object'},
-'location_address': {'rename': 'Address', 'dtype': 'object'},
-'location_city': {'rename': 'City', 'dtype': 'object'},
-'min_obligation_beg_date_yyyymmdd': {'rename': 'BegDateMin','dtype': 'datetime64'},
-'min_obligation_end_date_yyyymmdd': {'rename': 'EndDateMin','dtype': 'datetime64'},
-'max_obligation_end_date_yyyymmdd': {'rename': 'EndDateMax','dtype': 'datetime64'},
-'sum_total_receipts': {'rename': 'TotalSum', 'dtype': 'float64'},
-'avg_total_receipts': {'rename': 'TotalAvg', 'dtype': 'float64'},
-'sum_beer_receipts': {'rename': 'BeerSum', 'dtype': 'float64'},
-'avg_beer_receipts': {'rename': 'BeerAvg', 'dtype': 'float64'},
-'sum_wine_receipts': {'rename': 'WineSum', 'dtype': 'float64'},
-'avg_wine_receipts': {'rename': 'WineAvg', 'dtype': 'float64'},
-'sum_liquor_receipts': {'rename': 'LiqSum', 'dtype': 'float64'},
-'avg_liquor_receipts': {'rename': 'LiqAvg', 'dtype': 'float64'}
-})
-
-for col in df.columns:
-    
-    try:
-        temp_dict = cols[col]
-
-        df[col] = df[col].astype(temp_dict['dtype'])
+    for col in df.columns:
         
-    except:
-        pass
+        try:
+            temp_dict = cols[col]
 
-df = df.rename(columns={col:cols[col]['rename'] for col in cols})
+            df[col] = df[col].astype(temp_dict['dtype'])
+            
+        except:
+            pass
 
+    df = df.rename(columns={col:cols[col]['rename'] for col in cols})
 
-df.head()
+    print(df.head(1))
+
+    return df
+
+df_data = func_df_data(cities=['WEATHERFORD','FRISCO'])
+df_data.head()
 
 
 # %%
-# -*- coding: utf-8 -*-
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
+app.layout = html.Div([
+    html.Label('Multi-Select Dropdown'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label':city, 'value':city} for city in df_cities['City'].unique()],
+        value=['DALLAS'],
+        multi=True
+    ),
+    dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df_data.columns]
+)
 ])
+
+@app.callback(
+    Output('table', 'data'),
+    [Input('dropdown', 'value')])
+def update_table(value):
+
+    print(value)
+    
+    return func_df_data(value).to_dict('records')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
 
-
-
-# %%
