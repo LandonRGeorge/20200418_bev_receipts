be5f31d8a631cc3e683b1ac28144718247f7f271
LandonRGeorge <landon.robert.george@gmail.com>
2020-04-22 00:51:41 -0500

    Added segment sorting

--- main.py
+++ main.py
@@ -42,8 +42,15 @@ df_cities.head()
 
 # %%
 
-def func_df_data(cities):
+def func_df_data(cities, segment):
     """Grab data from TX Comptroller API"""
+
+    segment_dict = ({
+        'TOTAL':'sum_total_receipts',
+        'BEER':'sum_beer_receipts',
+        'WINE':'sum_wine_receipts',
+        'LIQUOR':'sum_liquor_receipts'
+    })
 
 
     text_cities = ''
@@ -53,31 +60,59 @@ def func_df_data(cities):
             text_temp+=','
         text_cities+=text_temp
 
-    url_args = f"""
-        $select=
-            location_name,
-            tabc_permit_number,
-            location_number,
-            location_address,
-            location_city,
-            min(obligation_end_date_yyyymmdd),
-            max(obligation_end_date_yyyymmdd),
-            sum(total_receipts),
-            sum(beer_receipts),
-            sum(wine_receipts),
-            sum(liquor_receipts)
-        &$group=
-            location_name,
-            tabc_permit_number,
-            location_number,
-            location_address,
-            location_city
-        &$where=
-            obligation_end_date_yyyymmdd between '2020-02-01' and '2020-04-30'
-            and location_city in({text_cities})
-        &$order=sum_liquor_receipts DESC
-        &$limit=100
-    """
+    if cities:
+
+        url_args = f"""
+            $select=
+                location_name,
+                tabc_permit_number,
+                location_number,
+                location_address,
+                location_city,
+                min(obligation_end_date_yyyymmdd),
+                max(obligation_end_date_yyyymmdd),
+                sum(total_receipts),
+                sum(beer_receipts),
+                sum(wine_receipts),
+                sum(liquor_receipts)
+            &$group=
+                location_name,
+                tabc_permit_number,
+                location_number,
+                location_address,
+                location_city
+            &$where=
+                obligation_end_date_yyyymmdd between '2020-02-01' and '2020-02-29'
+                and location_city in({text_cities})
+            &$order={segment_dict[segment]} DESC
+            &$limit=100
+        """
+
+    else:
+        url_args = f"""
+            $select=
+                location_name,
+                tabc_permit_number,
+                location_number,
+                location_address,
+                location_city,
+                min(obligation_end_date_yyyymmdd),
+                max(obligation_end_date_yyyymmdd),
+                sum(total_receipts),
+                sum(beer_receipts),
+                sum(wine_receipts),
+                sum(liquor_receipts)
+            &$group=
+                location_name,
+                tabc_permit_number,
+                location_number,
+                location_address,
+                location_city
+            &$where=
+                obligation_end_date_yyyymmdd between '2020-02-01' and '2020-02-29'
+            &$order={segment_dict[segment]} DESC
+            &$limit=100
+        """
 
 
     url_args = re.sub(r'\n\s*', '', url_args).replace(' ', '%20')
@@ -89,8 +124,8 @@ def func_df_data(cities):
             url_full, 
             convert_dates=['min_obligation_end_date_yyyymmdd', 'max_obligation_end_date_yyyymmdd']
         ).assign(
-            min_obligation_beg_date_yyyymmdd = lambda x: x['min_obligation_end_date_yyyymmdd'] - pd.offsets.MonthBegin()
-        )
+            min_obligation_end_date_yyyymmdd = lambda x: x['min_obligation_end_date_yyyymmdd'] - pd.offsets.MonthBegin()
+        ).rename(columns={'min_obligation_end_date_yyyymmdd':'min_obligation_beg_date_yyyymmdd'})
     )
 
     cols = ({
@@ -99,9 +134,8 @@ def func_df_data(cities):
     'location_number': {'rename': 'LocNbr', 'dtype': 'object'},
     'location_address': {'rename': 'Address', 'dtype': 'object'},
     'location_city': {'rename': 'City', 'dtype': 'object'},
-    'min_obligation_beg_date_yyyymmdd': {'rename': 'BegDateMin','dtype': 'datetime64'},
-    'min_obligation_end_date_yyyymmdd': {'rename': 'EndDateMin','dtype': 'datetime64'},
-    'max_obligation_end_date_yyyymmdd': {'rename': 'EndDateMax','dtype': 'datetime64'},
+    'min_obligation_beg_date_yyyymmdd': {'rename': 'BegDate','dtype': 'datetime64'},
+    'max_obligation_end_date_yyyymmdd': {'rename': 'EndDate','dtype': 'datetime64'},
     'sum_total_receipts': {'rename': 'TotalSum', 'dtype': 'float64'},
     'sum_beer_receipts': {'rename': 'BeerSum', 'dtype': 'float64'},
     'sum_wine_receipts': {'rename': 'WineSum', 'dtype': 'float64'},
@@ -120,11 +154,9 @@ def func_df_data(cities):
 
     df = df.rename(columns={col:cols[col]['rename'] for col in cols})
 
-    print(df.head(1))
-
     return df
 
-df_data = func_df_data(cities=['WEATHERFORD','FRISCO'])
+df_data = func_df_data(cities=[], segment='TOTAL')
 df_data.head()
 
 
@@ -134,12 +166,25 @@ df_data.head()
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.Label('Multi-Select Dropdown'),
+    html.H1('Mixed Beverage Gross Receipts'),
+    html.P('Dataset API can be found at data.texas.gov'),
+    html.Label('City Selection'),
     dcc.Dropdown(
-        id='dropdown',
+        id='selection-cities',
         options=[{'label':city, 'value':city} for city in df_cities['City'].unique()],
-        value=['DALLAS'],
+        value=[],
         multi=True
+    ),
+    html.Label('Sort By Segment Selection'),
+    dcc.RadioItems(
+        id='selection-segment',
+        options=[
+            {'label':'TOTAL', 'value':'TOTAL'},
+            {'label':'BEER', 'value':'BEER'},
+            {'label':'WINE', 'value':'WINE'},
+            {'label':'LIQUOR', 'value':'LIQUOR'}],
+        value='TOTAL',
+        labelStyle={'display': 'inline-block'}
     ),
     dash_table.DataTable(
     id='table',
@@ -149,12 +194,12 @@ app.layout = html.Div([
 
 @app.callback(
     Output('table', 'data'),
-    [Input('dropdown', 'value')])
-def update_table(value):
+    [Input('selection-cities', 'value'),
+     Input('selection-segment', 'value')])
+def update_table(cities, segment):
 
-    print(value)
     
-    return func_df_data(value).to_dict('records')
+    return func_df_data(cities, segment).to_dict('records')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
