75386697b0dca5ef2e7b19ea5533924e60fad95d
LandonRGeorge <landon.robert.george@gmail.com>
2020-05-07 08:04:24 -0500

    Added data bars, several formatting changes.

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def func_query_data(counties, cities, segment, retailer, start_date, end_date):
     url_args_where = func_url_args_where()
 
     url_args_order = f'&$order={segment} DESC'
-    url_args_limit = '&$limit=100'
+    url_args_limit = '&$limit=1000'
 
     url_args = url_args_select_group_where + url_args_where + url_args_order + url_args_limit
     url_args = re.sub(r'\n\s*', '', url_args).replace(' ', '%20')
@@ -156,7 +156,7 @@ def func_query_data(counties, cities, segment, retailer, start_date, end_date):
 query_data = func_query_data(counties=[], cities=[], segment='sum_total_receipts', retailer=None, start_date=start_date, end_date=end_date)
 
 
-def func_df_data(query_data):
+def func_df_data(query_data, segment):
     """Form dataframe from API query"""
 
     df = (
@@ -174,10 +174,10 @@ def func_df_data(query_data):
     'tabc_permit_number': {'rename': 'LicNbr', 'dtype': 'object'},
     'min_obligation_end_date_yyyymmdd': {'rename': 'BegDate','dtype': 'datetime64'},
     'max_obligation_end_date_yyyymmdd': {'rename': 'EndDate','dtype': 'datetime64'},
-    'sum_total_receipts': {'rename': 'TotalSum', 'dtype': 'float64'},
-    'sum_beer_receipts': {'rename': 'BeerSum', 'dtype': 'float64'},
-    'sum_wine_receipts': {'rename': 'WineSum', 'dtype': 'float64'},
-    'sum_liquor_receipts': {'rename': 'LiqSum', 'dtype': 'float64'},
+    'sum_total_receipts': {'rename': 'sum_total_receipts', 'dtype': 'float64'},
+    'sum_beer_receipts': {'rename': 'sum_beer_receipts', 'dtype': 'float64'},
+    'sum_wine_receipts': {'rename': 'sum_wine_receipts', 'dtype': 'float64'},
+    'sum_liquor_receipts': {'rename': 'sum_liquor_receipts', 'dtype': 'float64'},
     })
 
     for col in df.columns:
@@ -196,14 +196,60 @@ def func_df_data(query_data):
     df = df.rename(columns={col:cols[col]['rename'] for col in cols}).loc[:,cols_keep]
     df = df.assign(
         CountyDesc = lambda x: x['CountyNbr'].map(dict_county_map),
-        Rank = lambda x: x.index + 1,
+        Rank = lambda x: x[segment].rank(method='dense', ascending=False),
         BegDateStr = lambda x: x['BegDate'].dt.strftime('%Y-%m'),
         EndDateStr = lambda x: x['EndDate'].dt.strftime('%Y-%m')
         )
 
     return df
 
-df_data = func_df_data(query_data)
+df_data = func_df_data(query_data, segment='sum_total_receipts')
+
+
+
+def data_bars(df, column, segment):
+    """Add databars to Dash datatable, based on min and max values of column values."""
+
+    # color selected segment a certain color; non-selected columns take a different color
+    if column == segment:
+        color = '#5CDB95'
+        # color = '#0074D9' #blue
+    else:
+        color = '#cef4df'
+
+    n_bins = 100
+    bounds = [i * (1.0 / n_bins) for i in range(n_bins + 1)]
+    ranges = [
+        ((df[column].max() - df[column].min()) * i) + df[column].min()
+        for i in bounds
+    ]
+    styles = []
+    for i in range(1, len(bounds)):
+        min_bound = ranges[i - 1]
+        max_bound = ranges[i]
+        max_bound_percentage = bounds[i] * 100
+        styles.append({
+            'if': {
+                'filter_query': (
+                    '{{{column}}} >= {min_bound}' +
+                    (' && {{{column}}} < {max_bound}' if (i < len(bounds) - 1) else '')
+                ).format(column=column, min_bound=min_bound, max_bound=max_bound),
+                'column_id': column
+            },
+            'background': (
+                """
+                    linear-gradient(90deg,
+                    {color} 0%,
+                    {color} {max_bound_percentage}%,
+                    white {max_bound_percentage}%,
+                    white 100%)
+                """.format(max_bound_percentage=max_bound_percentage, color=color)
+            ),
+            'paddingBottom': 2,
+            'paddingTop': 2
+        })
+
+    return styles
 
 
 #%%
@@ -216,13 +262,7 @@ app.layout = html.Div([
         "This dashboard uses the ",
         html.Em("Texas Comptroller of Public Accounts' "),
         html.A("Mixed Beverage Gross Receipts data", href = "https://data.texas.gov/Government-and-Taxes/Mixed-Beverage-Gross-Receipts/naix-2893", target="_blank"),
-        ".",
-    ]),
-    html.P([
-        "Make selections below to modify the results.",
-    ]),
-    html.P([
-        "By default, the table shows the top 100 retailers, by total beverage gross receipts, for the state.",
+        ". By default, the table shows the top 100 retailers, by total beverage gross receipts, for the state.",
     ]),
     html.Div([
 
@@ -230,10 +270,10 @@ app.layout = html.Div([
         dcc.RadioItems(
             id='selection-segment',
             options=[
-                {'label':'Total', 'value':'sum_total_receipts'},
-                {'label':'Beer', 'value':'sum_beer_receipts'},
-                {'label':'Wine', 'value':'sum_wine_receipts'},
-                {'label':'Liquor', 'value':'sum_liquor_receipts'}],
+                {'label':'Total $', 'value':'sum_total_receipts'},
+                {'label':'Beer $', 'value':'sum_beer_receipts'},
+                {'label':'Wine $', 'value':'sum_wine_receipts'},
+                {'label':'Liquor $', 'value':'sum_liquor_receipts'}],
             value='sum_total_receipts',
             # labelStyle={'display': 'inline-block'}
         ),
@@ -280,7 +320,7 @@ app.layout = html.Div([
     dash_table.DataTable(
     id='table',
     columns=[
-        {'id': 'Rank', 'name': 'Rank', 'type': 'text'},
+        {'id': 'Rank', 'name': '', 'type': 'text'},
         {'id': 'LocName', 'name': 'Retailer Name', 'type': 'text'},
         {'id': 'Address', 'name': 'Address', 'type': 'text'},
         {'id': 'CountyDesc', 'name': 'County', 'type': 'text'},
@@ -288,37 +328,33 @@ app.layout = html.Div([
         {'id': 'LicNbr', 'name': 'License', 'type': 'text'},
         {'id': 'BegDateStr', 'name': 'Beg Date', 'type': 'datetime'},
         {'id': 'EndDateStr', 'name': 'End Date', 'type': 'datetime'},
-        {'id': 'TotalSum', 'name': 'Total $', 'type': 'numeric', 'format': FormatTemplate.money(0)},
-        {'id': 'BeerSum', 'name': 'Beer $', 'type': 'numeric', 'format': FormatTemplate.money(0)},
-        {'id': 'WineSum', 'name': 'Wine $', 'type': 'numeric', 'format': FormatTemplate.money(0)},
-        {'id': 'LiqSum', 'name': 'Liquor $', 'type': 'numeric', 'format': FormatTemplate.money(0)}
- ],
-    fixed_rows={'headers': False},
-    style_table={'height': 400},
+        {'id': 'sum_total_receipts', 'name': 'Total $', 'type': 'numeric', 'format': FormatTemplate.money(0)},
+        {'id': 'sum_beer_receipts', 'name': 'Beer $', 'type': 'numeric', 'format': FormatTemplate.money(0)},
+        {'id': 'sum_wine_receipts', 'name': 'Wine $', 'type': 'numeric', 'format': FormatTemplate.money(0)},
+        {'id': 'sum_liquor_receipts', 'name': 'Liquor $', 'type': 'numeric', 'format': FormatTemplate.money(0)}
+        ],
+    # page_size=20,
+    fixed_rows={'headers': True},
+    style_table={'height': 1000},
     style_cell={'textAlign': 'left'},
     style_cell_conditional=[
         {
             'if': {'column_id': c},
             'textAlign': 'right'
-        } for c in ['TotalSum', 'BeerSum','WineSum','LiqSum']
-    ],
-    style_data_conditional=[
-        {
-            'if': {'row_index': 'odd'},
-            'backgroundColor': 'rgb(248, 248, 248)'
-        }
+        } for c in ['sum_total_receipts', 'sum_beer_receipts','sum_wine_receipts','sum_liquor_receipts']
     ],
     style_header={
-        'backgroundColor': 'rgb(230, 230, 230)',
+        'backgroundColor': '#636363',
+        'color': 'white',
         'fontWeight': 'bold'
     },
-    # style_as_list_view=True, # removes vertical table dividers
+    style_as_list_view=True,
 )
 ])
 
-
 @app.callback(
-    Output('table', 'data'),
+    [Output('table', 'data'),
+    Output('table', 'style_data_conditional')],
     [Input('selection-counties', 'value'),
      Input('selection-cities', 'value'),
      Input('selection-segment', 'value'),
@@ -329,8 +365,18 @@ def update_table(counties, cities, segment, retailer, start_date, end_date):
     """Update datatable"""
 
     query_data = func_query_data(counties, cities, segment, retailer, start_date, end_date)
+    df_data = func_df_data(query_data, segment)
 
-    return func_df_data(query_data).to_dict('records')
+
+    style_data_conditional = (
+        data_bars(df_data, 'sum_total_receipts', segment) +
+        data_bars(df_data, 'sum_beer_receipts', segment) +
+        data_bars(df_data, 'sum_wine_receipts', segment) +
+        data_bars(df_data, 'sum_liquor_receipts', segment)
+    )
+
+
+    return df_data.to_dict('records'), style_data_conditional
 
 
 @app.callback(
@@ -341,14 +387,14 @@ def set_display_cities(counties, df=df_counties_cities):
 
     if counties:
 
-        cities = sorted(df[df['CountyNbr'].isin(counties)].loc[:,'City'].unique().tolist())
+        cities = df[df['CountyNbr'].isin(counties)].loc[:,'City'].unique().tolist()
 
     else:
 
-        cities = sorted(df['City'].unique().tolist())
+        cities = df['City'].unique().tolist()
 
-    return [{'label':city, 'value':city} for city in cities]
+    return [{'label':city, 'value':city} for city in sorted(cities)]
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
